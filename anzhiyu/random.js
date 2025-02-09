562a98e2bb1466f4125d036c840ca314135d584a
15166714520@163.com <15166714520@163.com>
2025-02-09 12:13:08 +0800

    Site updated: 2025-02-09 12:13:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/hello-world/"];function toRandomPost(){
+var posts=["2025/02/07/hello-world/","2025/02/09/qt/","2025/02/09/入门/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
