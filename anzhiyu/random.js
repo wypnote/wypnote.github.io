db723fabef5687e53a4c45d457187e615da78cb8
15166714520@163.com <15166714520@163.com>
2025-02-09 12:49:25 +0800

    Site updated: 2025-02-09 12:49:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/hello-world/","2025/02/09/qt/","2025/02/09/入门/"];function toRandomPost(){
+var posts=[];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
