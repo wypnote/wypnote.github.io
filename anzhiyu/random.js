db66d1b97318846d1d84ddc543bf7676b894721a
15166714520@163.com <15166714520@163.com>
2025-02-13 17:13:04 +0800

    Site updated: 2025-02-13 17:13:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/09/world/"];function toRandomPost(){
+var posts=["2025/02/09/next数组/","2025/02/09/world/","2025/02/09/文档/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
