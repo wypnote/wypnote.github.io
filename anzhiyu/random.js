850aad36efa4fab1cedd6721aafc2d934c22b067
15166714520@163.com <15166714520@163.com>
2025-02-26 00:07:20 +0800

    Site updated: 2025-02-26 00:07:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/19/Qt聊天室项目/","2025/02/09/next数组/","2025/02/24/IO多路转接（复用）之EPOLL/","2025/02/09/world/","2025/02/24/回声服务器/","2025/02/18/如何使用满血版deepseek/"];function toRandomPost(){
+var posts=["2025/02/24/IO多路转接（复用）之EPOLL/","2025/02/19/Qt聊天室项目/","2025/02/09/world/","2025/02/24/回声服务器/","2025/02/18/如何使用满血版deepseek/","2025/02/09/next数组/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
