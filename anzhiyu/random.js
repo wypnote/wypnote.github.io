bf69371f966703393dd78f74c877eddf38732f0d
15166714520@163.com <15166714520@163.com>
2025-03-07 15:27:32 +0800

    Site updated: 2025-03-07 15:27:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/19/Qt聊天室项目/","2025/02/18/如何使用满血版deepseek/"];function toRandomPost(){
+var posts=["2025/02/18/如何使用满血版deepseek/","2025/02/19/Qt聊天室项目/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
